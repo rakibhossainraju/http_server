7a034b7c2d5286e13a988b39359f94386ba5b7ed
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-01 00:03:14 +0600

    Refactor server to include TCP listener for connection handling and update address formatting

--- src/http/server.rs
+++ src/http/server.rs
@@ -1,17 +1,37 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     address: String,
-    port: u16,
 }
 
 impl Server {
     pub fn new(address: String, port: u16) -> Self {
-        Server { address, port }
+        Server { address: address + ":" + &port.to_string() }
     }
 
     pub fn start(&self) {
-        println!("Starting server at {}:{}", self.address, self.port);
+        let listener = TcpListener::bind(&self.address).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(size) => {
+                            println!("Received connection from {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(err) => {
+                            println!("Failed to read from stream: {}", err);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to accept connection: {}", e);
+                }
+            }
+        }
     }
     pub fn stop(self) {
-        println!("Stopping server at {}:{}", self.address, self.port);
+        println!("Stopping server at {}", self.address);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use http::Server;
 fn main() {
     let server = Server::new("127.0.0.1".to_string(), 8080);
     server.start();
-    server.stop();
+    // server.stop();
 }
