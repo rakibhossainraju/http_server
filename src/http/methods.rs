91b82e304f4de2b201f493f6dfcaaf455537f6fe
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-02 01:13:21 +0600

    Add from_str method to Method enum for string conversion

--- src/http/methods.rs
+++ src/http/methods.rs
@@ -10,3 +10,20 @@ pub enum Method {
     TRACE,
     CONNECT,
 }
+
+impl Method {
+    pub fn from_str(name: &str) -> Option<Self> {
+        match name {
+            "GET" => Some(Self::GET),
+            "POST" => Some(Self::POST),
+            "PUT" => Some(Self::PUT),
+            "DELETE" => Some(Self::DELETE),
+            "PATCH" => Some(Self::PATCH),
+            "HEAD" => Some(Self::HEAD),
+            "OPTIONS" => Some(Self::OPTIONS),
+            "TRACE" => Some(Self::TRACE),
+            "CONNECT" => Some(Self::CONNECT),
+            _ => None,
+        }
+    }
+}
