c8f98289336e2a8319950c7192986ae3f7fe5b20
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-01 21:46:11 +0600

    Add ParseError enum and implement TryFrom for Request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,25 @@
 use crate::http::Method;
 
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidRequest,
+    InvalidMethod,
+    InvalidPath,
+    InvalidQueryString,
+    InvalidProtocol,
+}
+
+
 pub struct Request {
     path: String,
     method: Method,
     query_string: Option<String>,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        todo!()
+    }
 }
