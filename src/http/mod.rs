d417e8f3a14bd0974b08f4f701e8f7cdd212bdf4
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-01 23:30:58 +0600

    Add parse_error module and update public exports in mod.rs

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,7 +1,9 @@
 pub mod request;
 pub mod server;
 mod methods;
+mod parse_error;
 
-pub use request::Request;
 pub use methods::Method;
+pub use parse_error::ParseError;
+pub use request::Request;
 pub use server::Server;
