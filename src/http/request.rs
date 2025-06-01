103278e6a73af4c0fed8af61d27e2f8f48349225
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-02 01:13:27 +0600

    Implement request parsing in try_from and add get_next_word helper method

--- src/http/request.rs
+++ src/http/request.rs
@@ -13,11 +13,25 @@ impl TryFrom<&[u8]> for Request {
 
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
         let raw_request = str::from_utf8(buffer)?;
+        let (method, query_string) = Self::get_next_word(raw_request).ok_or(ParseError::InvalidRequest)?;
+        let method = Method::from_str(method).ok_or(ParseError::InvalidMethod)?;
+
 
         Ok(Request {
             path: String::from("/"),
-            method: Method::GET,
-            query_string: None,
+            method,
+            query_string: if query_string == "" { None } else { Some(query_string.to_string()) },
         })
+    }
+}
+
+impl Request {
+    fn get_next_word(req_str: &str) -> Option<(&str, &str)> {
+        for (idx, char) in req_str.chars().enumerate() {
+            if char == ' ' {
+                return Some((&req_str[..idx], &req_str[idx + 1..]));
+            }
+        }
+        None
     }
 }
