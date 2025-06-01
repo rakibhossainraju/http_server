944fcca5a55eef56f5b6bd3d65dc012a3dc9873d
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-01 23:30:21 +0600

    Refactor request handling to separate connection processing and introduce ParseError enum

---
+++ src/http/parse_error.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidMethod,
+    InvalidPath,
+    InvalidQueryString,
+    InvalidProtocol,
+    InvalidEncoding,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "ParseError: {}", self.message())
+    }
+}
+
+impl Error for ParseError {}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid request format",
+            Self::InvalidMethod => "Invalid HTTP method",
+            Self::InvalidPath => "Invalid request path",
+            Self::InvalidQueryString => "Invalid query string format",
+            Self::InvalidProtocol => "Invalid HTTP protocol version",
+            Self::InvalidEncoding => "Invalid encoding in request",
+        }
+    }
+}
