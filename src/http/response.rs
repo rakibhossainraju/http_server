206846b6ec2f4a2e42e87a859601dcd710ba6572
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-03 18:43:14 +0600

    Enhance Response struct to support custom headers and content type

--- src/http/response.rs
+++ src/http/response.rs
@@ -7,6 +7,8 @@ use std::net::{Shutdown, TcpStream};
 pub struct Response {
     pub status_code: StatusCode,
     pub body: Option<String>,
+    pub content_type: Option<String>,
+    pub headers: Vec<(String, String)>,
 }
 
 impl Response {
@@ -14,27 +16,52 @@ impl Response {
         Response {
             status_code,
             body,
+            content_type: Some("text/html".to_string()),
+            headers: vec![],
         }
     }
+
     pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
-        write!(stream,
-               "HTTP/1.1 {} {}\r\n\r\n{}",
-               self.status_code,
-               self.status_code.reason_phrase(),
-               self.body.as_deref().unwrap_or("")
-        )
-            .and_then(|_| stream.flush())
-            .and_then(|_| stream.shutdown(Shutdown::Write))
+        let body = self.body.as_deref().unwrap_or("");
+        let content_type = self.content_type.as_deref().unwrap_or("text/html");
+
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\nContent-Type: {}\r\n",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            content_type,
+        )?;
+
+        for (name, value) in &self.headers {
+            write!(stream, "{}: {}\r\n", name, value)?;
+        }
+
+        write!(stream, "Content-Length: {}\r\n\r\n", body.len())?;
+        write!(stream, "{}", body)?;
+
+        stream.flush()?;
+        stream.shutdown(Shutdown::Write)
+    }
+
+    pub fn add_header(&mut self, headers: Vec<(String, String)>) {
+        self.headers = headers;
     }
 }
 
 impl Display for Response {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f,
-               "HTTP/1.1 {} {}\r\n\r\n{}",
-               self.status_code,
-               self.status_code.reason_phrase(),
-               self.body.as_deref().unwrap_or("")
+        let body = self.body.as_deref().unwrap_or("");
+        let content_type = self.content_type.as_deref().unwrap_or("text/html");
+
+        write!(
+            f,
+            "HTTP/1.1 {} {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            content_type,
+            body.len(),
+            body
         )
     }
 }
