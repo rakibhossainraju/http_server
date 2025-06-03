09ee5324450647af2af25b612bf1bf86695d2752
rakibhossainraju <rakibhossianraju@gmail.com>
2025-06-03 12:31:32 +0600

    Update server startup to initialize WebsiteHandler with public path

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use http::Server;
 use website_handler::WebsiteHandler;
 
 fn main() {
+    let public_path = env!("CARGO_MANIFEST_DIR").to_string() + "/public/";
     let server = Server::new("127.0.0.1".to_string(), 8080);
-    
-    server.start(WebsiteHandler{});
+    server.start(WebsiteHandler::new(public_path));
     // server.stop();
 }
